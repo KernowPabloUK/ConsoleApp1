7d365817feb7a304f623a60657fd60f6d65d7e58
KernowPabloUK <PaulMorganUK1@outlook.com>
2022-10-08 16:34:59 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersDivisibleByThree = 0;
+
+            for (int i = 1; i <= 100 ; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    numbersDivisibleByThree += 1 ;
+                }
+            }
+            Console.WriteLine($"{numbersDivisibleByThree}");
+
+            Console.WriteLine($"enter a number or OK to exit");
+            string breakCommand = "OK";
+            int numberSum = 0;
+            string userEntry = Console.ReadLine();
+
+            while (userEntry != breakCommand.ToUpper())
+            {
+                int userNumber = Convert.ToInt32(userEntry);
+                numberSum += userNumber;
+                Console.WriteLine($"enter a number or OK to exit");
+            }
+            Console.WriteLine($"Total sum = {numberSum}");
+
+
+
+
+
+
+        }
+    }
+}
